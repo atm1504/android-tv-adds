7a7f4a83b36f4d68bf8ca56e4189f0efd589a39a
Amartya Mondal <atm1504.in@gmail.com>
2022-06-24 13:25:32 +0530

    Stable code

--- app/src/main/java/com/example/doohtv/MainFragment.kt
+++ app/src/main/java/com/example/doohtv/MainFragment.kt
@@ -1,62 +1,54 @@
 package com.example.doohtv
 
-import java.util.Timer
-import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
-import androidx.leanback.app.BackgroundManager
-import androidx.leanback.app.BrowseSupportFragment
-import androidx.core.content.ContextCompat
-import android.util.DisplayMetrics
-import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.fragment.app.Fragment
 
 /**
  * Loads a grid of cards with movies to browse.
  */
-class MainFragment : BrowseSupportFragment() {
+class MainFragment : Fragment() {
 
-    private lateinit var mBackgroundManager: BackgroundManager
-    private var mDefaultBackground: Drawable? = null
-    private lateinit var mMetrics: DisplayMetrics
-    private var mBackgroundTimer: Timer? = null
+    val videoUrl = "https://c1x-digitalooh.s3.us-west-2.amazonaws.com/Coca-Cola+-+Happiness+Factory+(2006%2C+Netherlands).mp4"
+    private lateinit var videoView: VideoView
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        Log.i(TAG, "onCreate")
-        super.onActivityCreated(savedInstanceState)
-        prepareBackgroundManager()
-        setupUIElements()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy: " + mBackgroundTimer?.toString())
-        mBackgroundTimer?.cancel()
-    }
-
-    private fun prepareBackgroundManager() {
-
-        mBackgroundManager = BackgroundManager.getInstance(activity)
-        mBackgroundManager.attach(activity!!.window)
-        mDefaultBackground = ContextCompat.getDrawable(context!!, R.drawable.default_background)
-        mMetrics = DisplayMetrics()
-        activity!!.windowManager.defaultDisplay.getMetrics(mMetrics)
-    }
-
-    private fun setupUIElements() {
-        title = getString(R.string.browse_title)
-        // over title
-        headersState = BrowseSupportFragment.HEADERS_ENABLED
-        isHeadersTransitionOnBackEnabled = true
-
-        // set fastLane (or headers) background color
-        brandColor = ContextCompat.getColor(context!!, R.color.fastlane_background)
-        // set search icon color
-        searchAffordanceColor = ContextCompat.getColor(context!!, R.color.search_opaque)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+        videoView = rootView.findViewById(R.id.videoView)
+        return rootView
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Toast.makeText(context, "Testing it", Toast.LENGTH_LONG).show();
 
+        val uri: Uri = Uri.parse(videoUrl)
+        videoView.setVideoURI(uri)
 
+        val mediaController = MediaController(context)
+        mediaController.setAnchorView(videoView)
+        mediaController.setMediaPlayer(videoView)
+        videoView.setMediaController(mediaController)
 
+        videoView.start()
+
+    }
 
 
     companion object {
